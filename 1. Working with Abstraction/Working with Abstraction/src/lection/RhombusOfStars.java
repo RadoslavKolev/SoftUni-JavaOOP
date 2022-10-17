c3c477d4b190175183465ece7a04ea79dda11867
RadoslavKolev <radoslav.kolev02@gmail.com>
2022-10-17 12:20:12 +0300

    First Task - Done

---
+++ 1. Working with Abstraction/Working with Abstraction/src/lection/RhombusOfStars.java
@@ -0,0 +1,31 @@
+package lection;
+
+import java.util.Scanner;
+
+public class RhombusOfStars {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int size = Integer.parseInt(scanner.nextLine());
+
+        for (int starCount = 1; starCount <= size; starCount++) {
+            printRow(size, starCount);
+        }
+
+        for (int starCount = size - 1; starCount >= 1; starCount--) {
+            printRow(size, starCount);
+        }
+    }
+
+    private static void printRow(int size, int starCount) {
+        for (int i = 0; i < size - starCount; i++) {
+            System.out.print(" ");
+        }
+
+        for (int col = 1; col < starCount; col++) {
+            System.out.print("* ");
+        }
+
+        System.out.println("*");
+    }
+}
