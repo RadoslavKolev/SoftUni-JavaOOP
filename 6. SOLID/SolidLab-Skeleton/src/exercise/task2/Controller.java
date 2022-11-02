1f8385c88e91116ffebc06d426115cc7ab0d5ee5
RadoslavKolev <radoslav.kolev02@gmail.com>
2022-11-02 14:57:56 +0200

    FileAppender created

--- 6. SOLID/SolidLab-Skeleton/src/exercise/task2/Controller.java
+++ 6. SOLID/SolidLab-Skeleton/src/exercise/task2/Controller.java
@@ -2,16 +2,23 @@ package exercise.task2;
 
 import exercise.task2.appenders.Appender;
 import exercise.task2.appenders.ConsoleAppender;
+import exercise.task2.appenders.FileAppender;
 import exercise.task2.layouts.Layout;
 import exercise.task2.layouts.SimpleLayout;
 import exercise.task2.loggers.Logger;
 import exercise.task2.loggers.MessageLogger;
+import exercise.task2.utilities.File;
+import exercise.task2.utilities.LogFile;
 
 public class Controller {
     public static void main(String[] args) {
-        Layout layout = new SimpleLayout();
-        Appender appender = new ConsoleAppender(layout);
-        Logger logger = new MessageLogger(appender);
+        Layout simpleLayout = new SimpleLayout();
+        Appender consoleAppender = new ConsoleAppender(simpleLayout);
+
+        File file = new LogFile();
+        Appender fileAppender = new FileAppender(simpleLayout, file);
+
+        Logger logger = new MessageLogger(consoleAppender, fileAppender);
 
         logger.logError("3/26/2015 2:08:11 PM", "Error parsing JSON.");
         logger.logInfo("3/26/2015 2:08:11 PM", "User Pesho successfully registered.");
--- 6. SOLID/SolidLab-Skeleton/src/exercise/task2/appenders/FileAppender.java
+++ 6. SOLID/SolidLab-Skeleton/src/exercise/task2/appenders/FileAppender.java
@@ -1,10 +1,20 @@
 package exercise.task2.appenders;
 
 import exercise.task2.enums.LogLevel;
+import exercise.task2.layouts.Layout;
+import exercise.task2.utilities.File;
 
 public class FileAppender implements Appender{
+    private final Layout layout;
+    private final File file;
+
+    public FileAppender(Layout layout, File file) {
+        this.layout = layout;
+        this.file = file;
+    }
+
     @Override
     public void append(String timeStamp, LogLevel level, String message) {
-
+        file.write(layout.format(timeStamp, level, message));
     }
 }
