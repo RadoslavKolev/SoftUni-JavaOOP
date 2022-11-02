ebb5f1bc0a53210ef07eb4f27dad9b72d82f5be2
RadoslavKolev <radoslav.kolev02@gmail.com>
2022-11-02 14:43:53 +0200

    Replaced appender.append with function logToAll()

--- 6. SOLID/SolidLab-Skeleton/src/exercise/task2/loggers/MessageLogger.java
+++ 6. SOLID/SolidLab-Skeleton/src/exercise/task2/loggers/MessageLogger.java
@@ -4,34 +4,40 @@ import exercise.task2.appenders.Appender;
 import exercise.task2.enums.LogLevel;
 
 public class MessageLogger implements Logger {
-    private final Appender appender;
+    private final Appender[] appenders;
 
-    public MessageLogger(Appender appender) {
-        this.appender = appender;
+    public MessageLogger(Appender... appenders) {
+        this.appenders = appenders;
     }
 
     @Override
     public void logInfo(String timeStamp, String message) {
-        appender.append(timeStamp, LogLevel.INFO, message);
+        logToAll(timeStamp, LogLevel.INFO, message);
     }
 
     @Override
     public void logWarning(String timeStamp, String message) {
-        appender.append(timeStamp, LogLevel.WARNING, message);
+        logToAll(timeStamp, LogLevel.WARNING, message);
     }
 
     @Override
     public void logError(String timeStamp, String message) {
-        appender.append(timeStamp, LogLevel.ERROR, message);
+        logToAll(timeStamp, LogLevel.ERROR, message);
     }
 
     @Override
     public void logCritical(String timeStamp, String message) {
-        appender.append(timeStamp, LogLevel.CRITICAL, message);
+        logToAll(timeStamp, LogLevel.CRITICAL, message);
     }
 
     @Override
     public void logFatal(String timeStamp, String message) {
-        appender.append(timeStamp, LogLevel.FATAL, message);
+        logToAll(timeStamp, LogLevel.FATAL, message);
+    }
+
+    private void logToAll(String timeStamp, LogLevel level, String message) {
+        for (Appender appender : appenders) {
+            appender.append(timeStamp, level, message);
+        }
     }
 }
