5ac0427ce0aa5cb3da99b4eff198e02a52289cf8
RadoslavKolev <radoslav.kolev02@gmail.com>
2022-11-04 12:19:41 +0200

    Annotation Lection tasks - Done

---
+++ 7. Reflection and Annotation/Reflection and Annotations/src/lection/codingTracker/Tracker.java
@@ -0,0 +1,40 @@
+package lection.codingTracker;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Tracker {
+    @Author(name = "George")
+    public static void main(String[] args) {
+        Tracker.printMethodsByAuthor(Tracker.class);
+    }
+
+    @Author(name = "Peter")
+    public static void printMethodsByAuthor(Class<?> classInfo) {
+        Map<String, String> methodsByAuthor = new HashMap<>();
+        fillMap(classInfo, methodsByAuthor);
+        printMapElements(methodsByAuthor);
+    }
+
+    private static void fillMap(Class<?> classInfo, Map<String, String> methodsByAuthor) {
+        Method[] methods = classInfo.getDeclaredMethods();
+
+        for (Method method : methods) {
+            Author annotation = method.getAnnotation(Author.class);
+
+            if (annotation == null) continue;
+
+            String annotationName = annotation.name();
+            String methodName = method.getName() + "()";
+
+            methodsByAuthor.putIfAbsent(annotationName, methodName);
+        }
+    }
+
+    private static void printMapElements(Map<String, String> methodsByAuthor) {
+        for (var entry : methodsByAuthor.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue());
+        }
+    }
+}
---
+++ 7. Reflection and Annotation/Reflection and Annotations/src/lection/createAnnotation/TestClass.java
@@ -0,0 +1,6 @@
+package lection.createAnnotation;
+
+@Subject(categories = {"Test", "Annotations"})
+public class TestClass {
+    //TODO: Some logic
+}
